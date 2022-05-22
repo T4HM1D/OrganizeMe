e9d2ddf4c30e3f1f489f8de9e7149f730b6f83be
Tahmidol Alom <32310776+T4HM1D@users.noreply.github.com>
2022-05-22 21:54:25 +0100

    making script detect if its directly running from download folder or sub-folder

--- organise.py
+++ organise.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 # dictionary with key value pairs
 SUBDIRECTORIES = {
-    "DOCUMENTS": ['.pdf','.rtf','.txt'],
+    "DOCUMENTS": ['.pdf','.rtf','.txt','.docx','.pptx'],
     "AUDIO": ['.m4a','.m4b','.mp3'],
     "VIDEOS": ['.mov','.avi','.mp4'],
     "IMAGES": ['.jpg','.jpeg','.png','.gif']
@@ -16,16 +16,23 @@ def pickDictionary(ext): # takes in a extension type
     return "MISC" # if filetype doesn't exist in dictionary
 
 def organiseDirectory():
-    # cwd = Path()
-    for item in os.scandir(): # os.scandir iterator object all the items in the current working directory
-        if item.is_dir() or item.name == Path(__file__).name or item.name == ".gitignore": # if item is directory or the script itself, skip it
+    if not Path(__file__).absolute().name == 'Download':
+        cwd = Path(__file__).absolute().parents[1]
+        print('script is in a download sub-folder:', cwd)
+    else:
+        cwd = Path(__file__).absolute()
+        print('script is in download folder:', cwd)
+    for item in cwd.iterdir(): # os.scandir iterator object all the items in the current working directory / can also use pathlib.Path.itdir()
+        if item.is_dir() or item.name == Path(__file__).name or item.name == ".gitignore" or item.name == "README.md": # if item is directory or the script itself, skip it
             continue
         filePath = Path(item) # Returns relative path of the item.
         fileType = filePath.suffix.lower() # Returns the string value of the suffix of the items
         dictionary = pickDictionary(fileType) # calls pickDiectory function, returns the category of the filetype
-        dictionaryPath = Path(dictionary) # creates a path for the category.
+        dictionaryPath = cwd.joinpath(dictionary) # creates a path for the category.
         if not dictionaryPath.is_dir(): 
             dictionaryPath.mkdir() # If category directory doesn't exist, make it.
-        filePath.rename(dictionaryPath.joinpath(filePath)) # moves file into the category directory
-
+        newPath = dictionaryPath.joinpath(filePath.name)
+        # filePath.rename(newPath) # moves file into the category directory
+        print(Path(item))
+        print(newPath)
 organiseDirectory()
